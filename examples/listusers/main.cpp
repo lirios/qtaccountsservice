347b952b044df9555a0911f27a824a3eb4b2b6c2
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-07-18 13:52:11 +0200

    Add an example to list users

---
+++ examples/listusers/main.cpp
@@ -0,0 +1,45 @@
+/****************************************************************************
+ * This file is part of Qt AccountsService Addon.
+ *
+ * Copyright (C) 2015 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+
+#include <QtAccountsService/AccountsManager>
+#include <QtAccountsService/UserAccount>
+
+using namespace QtAccountsService;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    AccountsManager *manager = new AccountsManager();
+    UserAccountList list = manager->listCachedUsers();
+    Q_FOREACH (UserAccount *account, list)
+        qDebug() << account->realName() << account->userName();
+
+    return app.exec();
+}
