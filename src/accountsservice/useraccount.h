f777f8535ae6dae823c4e7674c2ac63f22d7d775
Leslie Zhai <xiangzhai83@gmail.com>
2015-08-31 10:07:06 +0800

    For sddm QT_NO_CAST_FROM_ASCII defined

--- src/accountsservice/useraccount.h
+++ src/accountsservice/useraccount.h
@@ -135,7 +135,7 @@ public:
     QString xsession() const;
     void setXSession(const QString &session);
 
-    void setPassword(const QString &password, const QString &hint = "");
+    void setPassword(const QString &password, const QString &hint = QString());
 
 Q_SIGNALS:
     void accountChanged();
